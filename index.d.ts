cbcd409d10d70abd6d282c22feb4604223167c64
Joynal Dsouza <joynaldsouza2@gmail.com>
2023-02-08 20:19:05 +0530

    feat : added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export type RollingNumberTickerProps = {
+  style?: object;
+  textSize?: number;
+  textStyle?: object;
+  fromNumber: number;
+  number: number;
+  duration?: number;
+  animationStartDelay?: number;
+};
